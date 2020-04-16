255cf97a6fb040a739979f81ed8ced14935bf76e
Sam Prinssen <samprinssen@gmail.com>
2020-04-16 15:25:17 +0200

    add mp-search

--- src/recensies-page/recensies-page.js
+++ src/recensies-page/recensies-page.js
@@ -1,6 +1,7 @@
 import { MPElement, html } from '../mp-element/mp-element';
 import '../recensie-preview/recensie-preview';
 import '../mp-page/mp-page';
+import '../mp-search/mp-search';
 import algoliasearch from 'https://cdn.jsdelivr.net/npm/algoliasearch@4/dist/algoliasearch-lite.esm.browser.js';
 
 class RecensiesPage extends MPElement {
@@ -9,32 +10,9 @@ class RecensiesPage extends MPElement {
     return { 
       items: {
         observe: true,
-        defaultValue: [1, 2, 3]
-      },
-      searchInput: {
-        observe: true,
-        defaultValue: "",
-        changedHandler: "_handleSearchInputChanged"
+        defaultValue: []
       }
     }
-  }
-
-  _handleSearchInputChanged(oldVal, newVal) {
-    console.log(oldVal, newVal)
-    if(!newVal) return;
-    this.runQuery(newVal)
-  }
-
-
-  constructor() {
-    super();
-    this.algoliaIndex = algoliasearch('QZ9LK09320', '5fe26edd91681f874040eb6110bf8a7f').initIndex('margriet_prinssen');
-    this.items = []
-  }
-
-  async runQuery(query, options = {}) {
-    const res = await this.algoliaIndex.search(query, options);
-    console.log(res)
   }
 
   get template() {
@@ -46,26 +24,27 @@ class RecensiesPage extends MPElement {
       <h1 slot="header-content">recensies</h1>
 
       <section>
-        <input type="text" @input="${(e) => {
-          console.log(e)
-          this.searchInput = e.target.value}}">
+        <mp-search 
+          algolia-config='{"applicationId": "QZ9LK09320","searchOnlyAPIKey": "5fe26edd91681f874040eb6110bf8a7f","index": "margriet_prinssen"}'
+          facet-attributes='["persons", "groups", "theaters"]'
+          @search-results-changed=${(e) => this.items = e.detail.value}
+        ></mp-search>
       </section>
+
       <section>
         <div class="recensie-grid">
           ${this.items.map(item => {
             return html`
-              ${Object.values(item)}
               <a href="#">
               <recensie-preview
-                title="Freudjes, geen familie"
-                groups='["mugmetdegoudentand"]'
-                time-performed="08-31-2007"
-                theater="Ruïne van Brederode"
-                city="Velsen"
-                title="Freudjes, geen familie"
-                actors='["Ab Gietelink", "Bert Apeldoorn", "Munda de la Marre", "Arthur Geesing"]'
-                directors='["Ab Gietelink"]'
-                writers='["J. W. Goethe", "Ab Gietelink"]'
+                .title="${item.title}"
+                .groups="${item.groups}"
+                .reviewDate="${item.reviewDate}"
+                .theater="${item.theaters}"
+                .city="${item.city}"
+                .actors="${item.persons}"
+                .directors="${item.directors}"
+                .writers="${item.writers}"
               >
               </recensie-preview>
               </a>
@@ -73,6 +52,7 @@ class RecensiesPage extends MPElement {
           })}
         </div>
       </section>
+
     </mp-page>
 
     
