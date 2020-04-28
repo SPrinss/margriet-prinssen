071f92b45093ea04b9487e0b0038bd1386f57549
Sam Prinssen <samprinssen@gmail.com>
2020-04-28 15:48:46 +0200

    fallback to title if name is not available

--- src/recensie-preview/recensie-preview.js
+++ src/recensie-preview/recensie-preview.js
@@ -68,7 +68,7 @@ class RecensiePreview extends MPElement {
 
     <header>
       <div class="header-main">
-        <h4>${this.name}</h4>
+        <h4>${this.name || this.title}</h4>
         ${this.groups && this.groups.length > 0 ? this.groups.map(group => {
           return html`
             <h5>${group}</h5>
