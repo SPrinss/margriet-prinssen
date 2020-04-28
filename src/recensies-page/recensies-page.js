acd11a4de087e028344951af686497754b852c13
Sam Prinssen <samprinssen@gmail.com>
2020-04-28 20:34:48 +0200

    only persons searchable

--- src/recensies-page/recensies-page.js
+++ src/recensies-page/recensies-page.js
@@ -57,7 +57,7 @@ class RecensiesPage extends MPElement {
         <mp-search 
           placeholder="Toneelschuur, Sander Plukaard, 1997, Macbeth ... "
           algolia-config='{"applicationId": "QZ9LK09320","searchOnlyAPIKey": "5fe26edd91681f874040eb6110bf8a7f","index": "reviews"}'
-          facet-attributes='["actors", "groups", "theater", "writers", "directors", "year", "city"]'
+          facet-attributes='["persons", "groups", "theater", "year", "city"]'
           @search-results-changed=${(e) => this.items = e.detail.value}
         ></mp-search>
       </section>
